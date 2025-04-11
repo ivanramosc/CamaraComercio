f46b09f56ebcebb8829a77fa6cb3f96154ba72ee
Ivan <49484159+ivanramosc@users.noreply.github.com>
2025-04-11 16:56:04 -0500

    Update db.php

--- db.php
+++ db.php
@@ -19,4 +19,10 @@ try {
 } catch (PDOException $e) {
     die("❌ Error de conexión: " . $e->getMessage());
 }
+    
+?>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
